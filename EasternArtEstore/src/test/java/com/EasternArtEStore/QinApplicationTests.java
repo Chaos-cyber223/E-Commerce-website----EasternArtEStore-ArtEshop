e0b94af10c5e4a017b77afefd870b59ba314550a
Qin Wang <qincorfu@gmail.com>
2024-01-17 00:40:56 -0500

    build maven

---
+++ EasternArtEstore/src/test/java/com/EasternArtEStore/QinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EasternArtEStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
